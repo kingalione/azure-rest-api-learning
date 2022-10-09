1bcad04659409d625d9b01ce01e57b4dc0b41532
Ali <akrdg1988@gmail.com>
2022-10-09 14:46:53 +0200

    implement deletePost endpoint

--- services/tableService.js
+++ services/tableService.js
@@ -57,6 +57,23 @@ const updateEntity = (tableName, entity) => {
   });
 };
 
+const deleteEntity = (tableName, entity) => {
+  return new Promise((resolve, reject) => {
+    tableSvc.deleteEntity(
+      tableName,
+      entity,
+      (error, result, response) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve();
+        }
+      }
+    );
+  });
+};
+
 exports.insertEntity = insertEntity;
 exports.queryEntities = queryEntities;
 exports.updateEntity = updateEntity;
+exports.deleteEntity = deleteEntity;
